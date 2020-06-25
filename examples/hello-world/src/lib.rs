ae882fc8c1cc4e0e1f0360e729f71d35cb3fc9c2
clearloop <udtrokia@gmail.com>
2020-06-26 01:42:06 +0800

    chore: update example with new syntax

--- examples/hello-world/src/lib.rs
+++ examples/hello-world/src/lib.rs
@@ -1,11 +1,16 @@
+use elvis::widgets::{layouts::Center, Text, TextStyle};
+use elvis_web::Widget;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(start)]
 pub fn run() {
-    let mut center = elvis_web::center(elvis_web::text(
-        Some("hello, world".into()),
-        None,
-    ));
+    let mut center = Widget::new(Center {
+        child: Text {
+            text: "hello, world".into(),
+            style: TextStyle::default(),
+        }
+        .into(),
+    });
 
     center.calling().unwrap();
 }
