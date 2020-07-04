8af9b50ea84228220859fd002929fd1a1cc08691
clearloop <udtrokia@gmail.com>
2020-07-04 15:45:40 +0800

    Generate Elvis APP in one command (#11)
    * feat(epm): batch app template while building
    
    * feat(epm): init elvis app in one command
    
    * perf(epm): remove build.sh in APP template
    
    * perf(epm): clean EOF in gitignore

--- crates/backend/examples/dir.rs
+++
@@ -1,8 +0,0 @@
-use elvis_backend::Crate;
-
-fn main() {
-    let pkg = Crate::new().unwrap();
-
-    pkg.build().unwrap();
-    pkg.bindgen().unwrap();
-}
--- crates/epm/src/lib.rs
+++ crates/epm/src/lib.rs
@@ -1,1 +1,2 @@
 pub mod cmds;
+mod template;
