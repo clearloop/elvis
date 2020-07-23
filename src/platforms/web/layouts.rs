805e0b5349b70eed5fb914adbdf4ddca641a69cd
clearloop <udtrokia@gmail.com>
2020-07-24 00:44:01 +0800

    perf(serde): remove commets of deserializing code

--- src/platforms/web/layouts.rs
+++ src/platforms/web/layouts.rs
@@ -1,27 +1,10 @@
-use crate::{
-    widgets::layouts::*,
-    widgets::values::{layouts::*, *},
-    Error, Node, Serde,
-};
+use crate::{widgets::layouts::*, widgets::values::layouts::*, Error, Node, Serde};
 
 /// serde single child widgets with match style
 macro_rules! ss {
     {$(($widget:ident, $style:ident),)*} => {
         $(
             impl Serde<$widget, String, Error> for $widget {
-                // fn de(s: String) -> Result<$widget, Error> {
-                //     let t = Node::de(s)?;
-                //     assert!(t.children.len() == 1);
-                //
-                //     let child = t.children[0].borrow().to_owned();
-                //     let style = t.attrs.get("style").unwrap_or(&"".to_string()).to_string();
-                //
-                //     Ok($widget {
-                //         child,
-                //         style: $style::de(style)?,
-                //     })
-                // }
-
                 fn ser(&self) -> String {
                     let t: Node = self.into();
                     t.ser()
@@ -37,20 +20,6 @@ macro_rules! sm {
     {$(($widget:ident, $style:ident),)*} => {
         $(
             impl Serde<$widget, String, Error> for $widget {
-                // fn de(s: String) -> Result<$widget, Error> {
-                //     let t = Node::de(s)?;
-                //
-                //     let children = t.children.iter().map(
-                //         |w| w.borrow().to_owned()
-                //     ).collect::<Vec<Node>>();
-                //     let style = t.attrs.get("style").unwrap_or(&"".to_string()).to_string();
-                //
-                //     Ok($widget {
-                //         children,
-                //         style: $style::de(style)?,
-                //     })
-                // }
-
                 fn ser(&self) -> String {
                     let t: Node = self.into();
                     t.ser()
@@ -72,51 +41,14 @@ sm! {
     (MultiColumn, MultiColumnStyle),
 }
 
-fn parse<'p>(s: &'p str) -> Vec<(&'p str, &'p str)> {
-    let mut attrs: Vec<(&str, &str)> = vec![];
-    s.split(';').collect::<Vec<&str>>().iter().for_each(|x| {
-        if let Some(m) = x.find(':') {
-            attrs.push((x[..m].trim(), &x[m..].trim()));
-        }
-    });
-
-    attrs
-}
-
 // styles
 impl Serde<AlignStyle, String, Error> for AlignStyle {
-    // fn de(s: String) -> Result<AlignStyle, Error> {
-    //     Ok(AlignStyle {
-    //         align: Alignments::de(s).unwrap_or(Alignments::Center),
-    //     })
-    // }
-
     fn ser(&self) -> String {
         self.align.ser()
     }
 }
 
 impl Serde<ContainerStyle, String, Error> for ContainerStyle {
-    // fn de(s: String) -> Result<ContainerStyle, Error> {
-    //     let mut cs = ContainerStyle::default();
-    //     let mut align = "".to_string();
-    //     parse(&s).iter().for_each(|(k, v)| match *k {
-    //         "height" => cs.height = Unit::de(v.to_string()).unwrap_or(Unit::Rem(1.0)),
-    //         "width" => cs.width = Unit::de(v.to_string()).unwrap_or(Unit::Rem(1.0)),
-    //         "padding" => cs.padding = Unit::de(v.to_string()).unwrap_or(Unit::Rem(0.0)),
-    //         "margin" => cs.margin = Unit::de(v.to_string()).unwrap_or(Unit::Rem(0.0)),
-    //         "background-color" => {
-    //             cs.background_color = Colors::de(v.to_string()).unwrap_or(Colors::Inherit)
-    //         }
-    //         "align-items" => align.push_str(&format!("align-items: {};", v.to_string())),
-    //         "justify-content" => align.push_str(&format!("justify-content: {};", v.to_string())),
-    //         _ => {}
-    //     });
-    //
-    //     cs.align = Alignments::de(align)?;
-    //     Ok(cs)
-    // }
-
     fn ser(&self) -> String {
         let mut s = "".to_string();
         s += &self.align.ser();
@@ -130,39 +62,6 @@ impl Serde<ContainerStyle, String, Error> for ContainerStyle {
 }
 
 impl Serde<FlexStyle, String, Error> for FlexStyle {
-    // fn de(s: String) -> Result<FlexStyle, Error> {
-    //     let mut fs = FlexStyle {
-    //         align: Alignments::Center,
-    //         basis: FlexBasis::Fill,
-    //         direction: FlexDirection::Column,
-    //         grow: Unit::None(1.0),
-    //         order: Unit::None(1.0),
-    //         wrap: true,
-    //     };
-    //
-    //     let mut align = "".to_string();
-    //     parse(&s).iter().for_each(|(k, v)| match *k {
-    //         "flex-basis" => fs.basis = FlexBasis::de(v.to_string()).unwrap_or(FlexBasis::Fill),
-    //         "flex-direction" => {
-    //             fs.direction = FlexDirection::de(v.to_string()).unwrap_or(FlexDirection::Column)
-    //         }
-    //         "flex-grow" => fs.grow = Unit::de(v.to_string()).unwrap_or(Unit::Percent(0.0)),
-    //         "flex-order" => fs.order = Unit::de(v.to_string()).unwrap_or(Unit::None(1.0)),
-    //         "flex-wrap" => {
-    //             fs.wrap = match *v {
-    //                 "wrap" => true,
-    //                 _ => false,
-    //             }
-    //         }
-    //         "align-items" => align.push_str(&format!("align-items: {};", v.to_string())),
-    //         "justify-content" => align.push_str(&format!("justify-content: {};", v.to_string())),
-    //         _ => {}
-    //     });
-    //
-    //     fs.align = Alignments::de(align)?;
-    //     Ok(fs)
-    // }
-
     fn ser(&self) -> String {
         let mut s = "".to_string();
         s += &self.align.ser();
@@ -176,35 +75,6 @@ impl Serde<FlexStyle, String, Error> for FlexStyle {
 }
 
 impl Serde<GridStyle, String, Error> for GridStyle {
-    // fn de(s: String) -> Result<GridStyle, Error> {
-    //     let mut gs = GridStyle {
-    //         col: GridAuto::Auto,
-    //         col_gap: Unit::None(0.0),
-    //         flow: GridFlow::Row,
-    //         row: GridAuto::Auto,
-    //         row_gap: Unit::None(0.0),
-    //         template_col: GridTemplate::None,
-    //         template_row: GridTemplate::None,
-    //     };
-    //
-    //     parse(&s).iter().for_each(|(k, v)| match *k {
-    //         "grid-auto-columns" => gs.col = GridAuto::de(v.to_string()).unwrap_or(GridAuto::Auto),
-    //         "grid-auto-flow" => gs.flow = GridFlow::de(v.to_string()).unwrap_or(GridFlow::Row),
-    //         "grid-auto-rows" => gs.row = GridAuto::de(v.to_string()).unwrap_or(GridAuto::Auto),
-    //         "grid-column-gap" => gs.col_gap = Unit::de(v.to_string()).unwrap_or(Unit::None(0.0)),
-    //         "grid-row-gap" => gs.row_gap = Unit::de(v.to_string()).unwrap_or(Unit::None(0.0)),
-    //         "grid-template-columns" => {
-    //             gs.template_col = GridTemplate::de(v.to_string()).unwrap_or(GridTemplate::None)
-    //         }
-    //         "grid-template-rows" => {
-    //             gs.template_row = GridTemplate::de(v.to_string()).unwrap_or(GridTemplate::None)
-    //         }
-    //         _ => {}
-    //     });
-    //
-    //     Ok(gs)
-    // }
-
     fn ser(&self) -> String {
         let mut ss = "".to_string();
 
@@ -224,28 +94,6 @@ impl Serde<GridStyle, String, Error> for GridStyle {
 }
 
 impl Serde<MultiColumnStyle, String, Error> for MultiColumnStyle {
-    // fn de(s: String) -> Result<MultiColumnStyle, Error> {
-    //     let mut mc = MultiColumnStyle {
-    //         color: Colors::Inherit,
-    //         count: Unit::Auto,
-    //         gap: Unit::Auto,
-    //         style: MultiColumnLineStyle::None,
-    //     };
-    //
-    //     parse(&s).iter().for_each(|(k, v)| match *k {
-    //         "column-count" => mc.count = Unit::de(v.to_string()).unwrap_or(Unit::Auto),
-    //         "column-gap" => mc.gap = Unit::de(v.to_string()).unwrap_or(Unit::Auto),
-    //         "column-rule-color" => mc.color = Colors::de(v.to_string()).unwrap_or(Colors::Inherit),
-    //         "column-rule-style" => {
-    //             mc.style =
-    //                 MultiColumnLineStyle::de(v.to_string()).unwrap_or(MultiColumnLineStyle::None)
-    //         }
-    //         _ => {}
-    //     });
-    //
-    //     Ok(mc)
-    // }
-
     fn ser(&self) -> String {
         let mut ss = "".to_string();
         ss.push_str(&format!("column-count: {}", self.count.ser()));
@@ -257,21 +105,6 @@ impl Serde<MultiColumnStyle, String, Error> for MultiColumnStyle {
 }
 
 impl Serde<SizedBoxStyle, String, Error> for SizedBoxStyle {
-    // fn de(s: String) -> Result<SizedBoxStyle, Error> {
-    //     let mut sbs = SizedBoxStyle {
-    //         height: Unit::Auto,
-    //         width: Unit::Auto,
-    //     };
-    //
-    //     parse(&s).iter().for_each(|(k, v)| match *k {
-    //         "height" => sbs.height = Unit::de(v.to_string()).unwrap_or(Unit::Auto),
-    //         "width" => sbs.width = Unit::de(v.to_string()).unwrap_or(Unit::Auto),
-    //         _ => {}
-    //     });
-    //
-    //     Ok(sbs)
-    // }
-
     fn ser(&self) -> String {
         format!(
             "height: {}; width: {};",
@@ -283,35 +116,6 @@ impl Serde<SizedBoxStyle, String, Error> for SizedBoxStyle {
 
 // enums
 impl Serde<Alignments, String, Error> for Alignments {
-    // fn de(s: String) -> Result<Alignments, Error> {
-    //     let ss = s.split(';').collect::<Vec<&str>>();
-    //     assert!(ss.len() == 2);
-    //
-    //     let ai = ss[0][ss[0].find(':').unwrap_or(0)..].trim();
-    //     let jc = ss[1][ss[1].find(':').unwrap_or(0)..].trim();
-    //
-    //     Ok(match ai {
-    //         "flex-end" => match jc {
-    //             "center" => Alignments::BottomCenter,
-    //             "flex-start" => Alignments::BottomLeft,
-    //             "flex-end" => Alignments::BottomRight,
-    //             _ => Alignments::Center,
-    //         },
-    //         "center" => match jc {
-    //             "flex-start" => Alignments::CenterLeft,
-    //             "flex-end" => Alignments::CenterRight,
-    //             _ => Alignments::Center,
-    //         },
-    //         "flex-start" => match jc {
-    //             "center" => Alignments::TopCenter,
-    //             "flex-start" => Alignments::TopLeft,
-    //             "flex-end" => Alignments::TopRight,
-    //             _ => Alignments::Center,
-    //         },
-    //         _ => Alignments::Center,
-    //     })
-    // }
-
     fn ser(&self) -> String {
         let (ai, jc) = match self {
             Alignments::BottomCenter => ("flex-end", "center"),
@@ -330,19 +134,6 @@ impl Serde<Alignments, String, Error> for Alignments {
 }
 
 impl Serde<FlexBasis, String, Error> for FlexBasis {
-    // fn de(s: String) -> Result<FlexBasis, Error> {
-    //     let kv = s.split(':').collect::<Vec<&str>>();
-    //     assert!(kv.len() == 2);
-    //
-    //     Ok(match kv[1].trim() {
-    //         "fill" => FlexBasis::Fill,
-    //         "fit-content" => FlexBasis::FitContent,
-    //         "max-content" => FlexBasis::MaxContent,
-    //         "min-content" => FlexBasis::MinContent,
-    //         x => FlexBasis::Number(Unit::de(x.to_string()).unwrap_or(Unit::Auto)),
-    //     })
-    // }
-
     fn ser(&self) -> String {
         format!(
             "flex-basis: {};",
@@ -358,19 +149,6 @@ impl Serde<FlexBasis, String, Error> for FlexBasis {
 }
 
 impl Serde<FlexDirection, String, Error> for FlexDirection {
-    // fn de(s: String) -> Result<FlexDirection, Error> {
-    //     let kv = s.split(':').collect::<Vec<&str>>();
-    //     assert!(kv.len() == 2);
-    //
-    //     Ok(match kv[1].trim() {
-    //         "column" => FlexDirection::Column,
-    //         "column-reverse" => FlexDirection::ColumnReverse,
-    //         "row" => FlexDirection::Row,
-    //         "row-reverse" => FlexDirection::RowReverse,
-    //         _ => FlexDirection::Column,
-    //     })
-    // }
-
     fn ser(&self) -> String {
         format!(
             "flex-direction: {};",
@@ -385,41 +163,6 @@ impl Serde<FlexDirection, String, Error> for FlexDirection {
 }
 
 impl Serde<GridAuto, String, Error> for GridAuto {
-    // fn de(s: String) -> Result<GridAuto, Error> {
-    //     let v = s.split(':').collect::<Vec<&str>>()[1].trim();
-    //     Ok(match v {
-    //         "auto" => GridAuto::Auto,
-    //         "max-content" => GridAuto::MaxContent,
-    //         "min-content" => GridAuto::MinContent,
-    //         "inherit" => GridAuto::Inherit,
-    //         "initial" => GridAuto::Initial,
-    //         "unset" => GridAuto::Unset,
-    //         m if m.contains("minmax") => {
-    //             if let Some(i) = m.find('(') {
-    //                 let cr = m[i..(m.len() - 1)].split(',').collect::<Vec<&str>>();
-    //                 assert!(cr.len() == 2);
-    //
-    //                 GridAuto::MinMax(
-    //                     Unit::de(cr[0].trim().to_string())?,
-    //                     Unit::de(cr[1].trim().to_string())?,
-    //                 )
-    //             } else {
-    //                 GridAuto::Unset
-    //             }
-    //         }
-    //         x if x.trim().contains(|w: char| w.is_whitespace()) => {
-    //             let mut o: Vec<Unit> = vec![];
-    //             x.split(|c: char| c.is_whitespace())
-    //                 .collect::<Vec<&str>>()
-    //                 .iter()
-    //                 .for_each(|u| o.push(Unit::de(u.to_string()).unwrap_or(Unit::Auto)));
-    //
-    //             GridAuto::Plain(o)
-    //         }
-    //         u => GridAuto::Fixed(Unit::de(u.to_string()).unwrap_or(Unit::Auto)),
-    //     })
-    // }
-
     fn ser(&self) -> String {
         match self {
             GridAuto::Auto => "auto".to_string(),
@@ -444,20 +187,6 @@ impl Serde<GridAuto, String, Error> for GridAuto {
 }
 
 impl Serde<GridFlow, String, Error> for GridFlow {
-    // fn de(s: String) -> Result<GridFlow, Error> {
-    //     let v = s.split(':').collect::<Vec<&str>>()[1].trim();
-    //     Ok(match v {
-    //         "column" => GridFlow::Column,
-    //         "column dense" => GridFlow::ColumnDense,
-    //         "dense" => GridFlow::Dense,
-    //         "inherit" => GridFlow::Inherit,
-    //         "initial" => GridFlow::Initial,
-    //         "row" => GridFlow::Row,
-    //         "row dense" => GridFlow::RowDense,
-    //         _ => GridFlow::Unset,
-    //     })
-    // }
-
     fn ser(&self) -> String {
         match self {
             GridFlow::Column => "column",
@@ -474,61 +203,6 @@ impl Serde<GridFlow, String, Error> for GridFlow {
 }
 
 impl Serde<GridTemplate, String, Error> for GridTemplate {
-    // fn de(s: String) -> Result<GridTemplate, Error> {
-    //     let kv = s.split(':').collect::<Vec<&str>>();
-    //     assert!(kv.len() == 2);
-    //
-    //     Ok(match kv[1].trim() {
-    //         "inherit" => GridTemplate::Inherit,
-    //         "initial" => GridTemplate::Initial,
-    //         "subgrid" => GridTemplate::SubGrid,
-    //         "unset" => GridTemplate::Unset,
-    //         "none" => GridTemplate::None,
-    //         x if x.contains("fit-content") => {
-    //             if let Some(i) = x.find('(') {
-    //                 GridTemplate::FitContent(Unit::de(x[i..(x.len() - 1)].trim().to_string())?)
-    //             } else {
-    //                 GridTemplate::Unset
-    //             }
-    //         }
-    //         m if m.contains("minmax") => {
-    //             if let Some(i) = m.find('(') {
-    //                 let ai = m[i..(m.len() - 1)].split(',').collect::<Vec<&str>>();
-    //                 assert!(ai.len() == 2);
-    //
-    //                 GridTemplate::MinMax(
-    //                     Unit::de(ai[0].trim().to_string())?,
-    //                     Unit::de(ai[1].trim().to_string())?,
-    //                 )
-    //             } else {
-    //                 GridTemplate::Unset
-    //             }
-    //         }
-    //         _ => {
-    //             if let Some(b) = kv[1].find('(') {
-    //                 let nv = kv[1][b..(kv[1].len() - 1)]
-    //                     .split(',')
-    //                     .collect::<Vec<&str>>();
-    //                 assert!(nv.len() == 2);
-    //
-    //                 GridTemplate::Repeat(
-    //                     nv[0].parse().unwrap_or(1),
-    //                     Unit::de(nv[1].to_string()).unwrap_or(Unit::Auto),
-    //                 )
-    //             } else {
-    //                 let mut o: Vec<Unit> = vec![];
-    //                 kv[1]
-    //                     .split(|x: char| x.is_whitespace())
-    //                     .collect::<Vec<&str>>()
-    //                     .iter()
-    //                     .for_each(|x| o.push(Unit::de(x.to_string()).unwrap_or(Unit::Auto)));
-    //
-    //                 GridTemplate::Plain(o)
-    //             }
-    //         }
-    //     })
-    // }
-
     fn ser(&self) -> String {
         match self {
             GridTemplate::FitContent(u) => format!("fit-content({})", u.ser()),
@@ -553,24 +227,6 @@ impl Serde<GridTemplate, String, Error> for GridTemplate {
 }
 
 impl Serde<MultiColumnLineStyle, String, Error> for MultiColumnLineStyle {
-    // fn de(s: String) -> Result<MultiColumnLineStyle, Error> {
-    //     let kv = s.split(':').collect::<Vec<&str>>();
-    //     assert!(kv.len() == 2);
-    //
-    //     Ok(match kv[1] {
-    //         "none" => MultiColumnLineStyle::None,
-    //         "dotted" => MultiColumnLineStyle::Dotted,
-    //         "double" => MultiColumnLineStyle::Double,
-    //         "groove" => MultiColumnLineStyle::Groove,
-    //         "hidden" => MultiColumnLineStyle::Hidden,
-    //         "inset" => MultiColumnLineStyle::Inset,
-    //         "outset" => MultiColumnLineStyle::OutSet,
-    //         "ridge" => MultiColumnLineStyle::Ridge,
-    //         "solid" => MultiColumnLineStyle::Solid,
-    //         _ => MultiColumnLineStyle::None,
-    //     })
-    // }
-
     fn ser(&self) -> String {
         format!(
             "style: {};",
--- src/platforms/web/values.rs
+++ src/platforms/web/values.rs
@@ -2,23 +2,8 @@ use crate::{
     widgets::values::{Colors, Unit},
     Error, Serde,
 };
-use std::str::FromStr;
 
 impl Serde<Colors, String, Error> for Colors {
-    // fn de(s: String) -> Result<Colors, Error> {
-    //     if s.contains("inherit") {
-    //         return Ok(Colors::Inherit);
-    //     }
-    //
-    //     let vs: Vec<&str> = s[5..s.len() - 1].split(',').collect();
-    //     Ok(Colors::ORGB(
-    //         vs[3].trim().parse().unwrap_or(0.0),
-    //         vs[0].trim().parse().unwrap_or(0),
-    //         vs[1].trim().parse().unwrap_or(0),
-    //         vs[2].trim().parse().unwrap_or(0),
-    //     ))
-    // }
-
     fn ser(&self) -> String {
         match self {
             Colors::ORGB(o, r, g, b) => format!("rgba({}, {}, {}, {:.1})", r, g, b, o),
@@ -35,10 +20,6 @@ impl Serde<Colors, String, Error> for Colors {
 }
 
 impl Serde<Unit, String, Error> for Unit {
-    // fn de(s: String) -> Result<Unit, Error> {
-    //     Unit::from_str(&s)
-    // }
-
     fn ser(&self) -> String {
         self.to_string()
     }
